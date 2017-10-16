269e9dff739f992dd47e1c237f22b2c46096356f
Tiago Bandeira <tiagobandeirabarros@gmail.com>
2017-10-16 11:45:32 -0300

    Ajustes Gerais

--- view/administrador.php
+++ view/administrador.php
@@ -23,7 +23,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
     <meta name="description" content="">
     <meta name="author" content="Dashboard">
     <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
