c3b0bb8e402f9492dcf050e04a512fb4652aa7b6
Tiago Bandeira <tiagobandeirabarros@gmail.com>
2017-12-18 20:27:58 -0300

    Reinstalando

--- database/install.php
+++ database/install.php
@@ -7,16 +7,18 @@
 
 	*/
 	function dataConnect(){
-		/*
+		
 		$host = "mysql.hostinger.com.br";
 		$db = "u599766642_even";
 		$user = "u599766642_tiago";
 		$password = "i6d3tUWfH5Ax";
-		*/
+		
+		/*
 		$host = "127.0.0.1";
 		$db = "sisevento";
 		$user = "root";
 		$password = "tiagoadmin";
+		*/
 		$data = array();
 		array_push($data, $host);
 		array_push($data, $db);
